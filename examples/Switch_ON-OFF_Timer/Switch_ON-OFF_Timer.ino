34a5dda4bd9a7d0c715787fd678e6e046b7920fa
rferrari81 <rferrari81@users.noreply.github.com>
2021-01-03 20:11:25 +0100

    Add files via upload

---
+++ examples/Switch_ON-OFF_Timer/Switch_ON-OFF_Timer.ino
@@ -0,0 +1,13 @@
+#include <oneButtonSwitch.h>
+
+#define pinButton 10     //ON pin onebuttonSwitch board
+#define pinHoldMOSFET 7  //OFF pin onebuttonSwitch board
+
+oneButtonSwitch OBS(pinButton, pinHoldMOSFET, 5000);
+
+void setup() {
+}
+
+void loop() {
+  OBS.oneButtonSwitchLOOP();
+}
