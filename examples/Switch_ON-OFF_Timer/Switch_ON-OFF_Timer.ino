857c8cd8b61786a94cbc75bece9d75eba6a32121
rferrari81 <rferrari81@users.noreply.github.com>
2021-01-21 18:11:41 +0100

    Update Switch_ON-OFF_Timer.ino

--- examples/Switch_ON-OFF_Timer/Switch_ON-OFF_Timer.ino
+++ examples/Switch_ON-OFF_Timer/Switch_ON-OFF_Timer.ino
@@ -1,7 +1,7 @@
 #include <oneButtonSwitch.h>
 
-#define pinButton 10     //ON pin onebuttonSwitch board
-#define pinHoldMOSFET 7  //OFF pin onebuttonSwitch board
+#define pinButton 10     //OFF pin onebuttonSwitch board
+#define pinHoldMOSFET 7  //ON pin onebuttonSwitch board
 
 oneButtonSwitch OBS(pinButton, pinHoldMOSFET, 5000);
 
